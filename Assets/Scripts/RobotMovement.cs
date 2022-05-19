76556e2dec3d79d95bc6f9c45bbc8a3d65366663
Bizubani <bizubani@gmail.com>
2022-05-19 18:00:08 -0400

    Added a gitIgnore file to reduce pull request size.

--- Assets/Scripts/RobotMovement.cs
+++ Assets/Scripts/RobotMovement.cs
@@ -9,37 +9,40 @@ public class RobotMovement : MonoBehaviour
     [SerializeField] GameObject robotTurret;
     [SerializeField] float moveSpeed;
     [SerializeField] float rotateSpeed = 3.0f;
-    
+
+    const string _horizontal = "Horizontal";
+    const string _vertical = "Vertical";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Get Axis input, used to control movement via WASD keys.
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis(_horizontal);
+        float v = Input.GetAxis(_vertical);
         //Get Raw input for rotation of robot player.
-        float bodyRotationH = Input.GetAxisRaw("Horizontal");
-        float bodyRotationV = Input.GetAxisRaw("Vertical");
+        float bodyRotationH = Input.GetAxisRaw(_horizontal);
+        float bodyRotationV = Input.GetAxisRaw(_vertical);
 
         //Get the target rotation direction and rotate towards that as speed determined rotateSpeed variable.
-        Vector3 targetDirection = new Vector3(bodyRotationH, 0, bodyRotationV); 
+        Vector3 targetDirection = new Vector3(bodyRotationH, 0, bodyRotationV);
         Vector3 newDirection = Vector3.RotateTowards(robotPlayer.transform.forward, targetDirection, rotateSpeed * Time.deltaTime, 0.0f);
         robotPlayer.transform.rotation = Quaternion.LookRotation(newDirection);
-        
+
         //Move the player globally via WASD input
-        transform.Translate(new Vector3 (h, 0, v) * moveSpeed * Time.deltaTime, Space.World);
-        
+        transform.Translate(new Vector3(h, 0, v) * moveSpeed * Time.deltaTime, Space.World);
+
         //Create an invisable Ray from the camera to the mouse cursor.
         //Also creates a new plane as the height of the robot turret, this is use to rotate the turret towards the mouse cursor.
         Ray mouseRay = playerCamera.ScreenPointToRay(Input.mousePosition);
         Plane p = new Plane(Vector3.up, robotTurret.transform.position);
 
-        if(p.Raycast(mouseRay, out float hitDist))
+        if (p.Raycast(mouseRay, out float hitDist))
         {
             Vector3 hitPoint = mouseRay.GetPoint(hitDist);
             robotTurret.transform.LookAt(hitPoint);
